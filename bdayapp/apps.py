dc9581e546622a2fbdf72add419c7f1f8f5ddf8b
rajugoud18 <121607326+rajugoud18@users.noreply.github.com>
2023-01-08 21:43:48 +0530

    Add files via upload

---
+++ bdayapp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class BdayappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'bdayapp'
---
+++ bdayapp/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+def Bday(request):
+    return render(request,'bady.html')
+def Hpybday(request):
+    return render(request,'bady2.html')
